7513bb7b307368d94958678ac0a70c7b14df4301
JuanLariasMendez <jlariasm@miumg.edu.gt>
2022-09-19 20:29:01 -0600

    cambios

--- agregarmascota.php
+++ agregarmascota.php
@@ -8,21 +8,10 @@
     <title>Document</title>
 </head>
 <body>
-    <div>
-        <nav class="navbar navbar-expand-lg bg-light">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="">HOME</a></li>
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Home</a></li>
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="agregarmascota.php">Agregar Mascota</a></li>
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="listarmascota.php">Listar Mascotas</a></li>
-            </ul>
-
-        </nav>
-    </div>
     <p>
         <h2 style="text-align: center">INGRESO DE DATOS DE LA MASCOTA</h2>
     </p>
-    <form {margin: 0 auto;} action="mascotacontroller.php" method="post">
+    <form {margin: 0 auto;} action="VacunasController.php" method="post">
         <p>
             <label for="nombre">Nombre:</label>
             <input type="text" class = "form-control" name="nombre">
