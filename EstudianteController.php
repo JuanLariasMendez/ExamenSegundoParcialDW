2cc2400f2f1ac21295a532b73481ae811a44a977
JuanLariasMendez <jlariasm@miumg.edu.gt>
2022-09-19 19:11:05 -0600

    Mensaje

---
+++ EstudianteController.php
@@ -0,0 +1,25 @@
+<?php
+include "EstudianteModel.php";
+$nuevoEstudiante = new Estudiante();
+
+/*GUARDAR*/
+if(isset($_POST['btnGuardar']))
+{
+    $nuevoEstudiante->GuardarEstudiante($_POST['apellidos'],$_POST['nombre'],$_POST['direccion'],$_POST['telefono']);
+    header('Location: VistaEstudiante.php');
+}
+else
+if(isset($_POST['btnEditar']))
+{
+ $nuevoEstudiante->EditarEstudiante($_POST['idStudent'],$_POST['txtapellidos'],$_POST['txtnombre'],$_POST['txtdireccion'],$_POST['txttelefono']); 
+ header('Location: VistaEstudiante.php');
+        
+}
+if(isset($_POST['btnEliminar']))
+{
+ $nuevoEstudiante->EliminarEstudiante($_POST['idStudent'],$_POST['txtapellidos'],$_POST['txtnombre'],$_POST['txtdireccion'],$_POST['txttelefono']); 
+ header('Location: VistaEstudiante.php');
+        
+}
+
+/*EDITAR*/
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+    <a href="VistaEstudiante.php">Ir a Vista Estudiante</a>
+    <p>Amilcar Larias....Prueba de Sincronización</p>
+
+    <div id="menu">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Agregar Mascota</a></li>
+            <li><a href="#">Lista Mascota</a></li>
+        <ul>
+    <div>
+</body>
+</html>
